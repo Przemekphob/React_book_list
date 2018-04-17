33dbbd522de0c24c38ec2cacfc7adf6a07ca337e
Przemek Mazur <Przemek1713@gmail.com>
2018-04-17 20:20:50 +0200

    added book detail and finish app

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{ title: '1st book' },
-		{ title: '2nd book' },
-		{ title: '3rd book' },
-		{ title: '4th book' }						
+		{ title: '1st book', pages: 10 },
+		{ title: '2nd book', pages: 15 },
+		{ title: '3rd book', pages: 2 },
+		{ title: '4th book', pages: 1 }						
 	];
 }
